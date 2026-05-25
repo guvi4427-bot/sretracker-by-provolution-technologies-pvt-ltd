171acabd51f32b5487869a09be3ca2142df77f98
Z User <z@container>
2026-05-25 17:18:28 +0000

    6ec7b215-4186-44f7-9a28-3496e87a435b

--- workspace-extract/src/app/layout.tsx
+++ workspace-extract/src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7745236489664493" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7745236489664493"
